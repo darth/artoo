e714561db7cedfbf6276b0f3039624e634b7068b
Vadim Alimguzhin <vadim.alimguzhin@gmail.com>
2021-10-16 18:51:11 +0200

    More fixes to update handler.

--- update.js
+++ update.js
@@ -2,7 +2,7 @@ module.exports = async (context, cmdName, ...text) => {
   if (text.length > 0) {
     const cmd = await context.commands.get(cmdName);
     if (cmd && cmd.handler === "text") {
-      cmd.args = text.slice(1).join(" ").split("\n");
+      cmd.args = text.join(" ").split("|");
       context.commands.set(cmdName, cmd);
     }
   }
