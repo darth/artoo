382e48769907c347581f634a7b9a59f7d184a130
Vadim Alimguzhin <vadim.alimguzhin@gmail.com>
2022-08-27 19:05:06 +0200

    Fix update handler.

--- counter.js
+++ counter.js
@@ -2,5 +2,5 @@ const pluralize = require("pluralize");
 
 module.exports = async (context, text, num) => {
   const numStr = pluralize("time", num, true);
-  await context.chat.say(context.channel.displayName, `${text} ${numStr}.`);
+  await context.chat.say(context.channel.displayName, `${text} ${numStr}!`);
 };
--- update.js
+++ update.js
@@ -1,21 +1,21 @@
 module.exports = async (context, cmdName, ...args) => {
-  if (text.length > 0) {
-    const cmd = await context.commands.get(cmdName);
-    if (cmd) {
-      if (cmd.handler === "text") {
-        cmd.args = text.join(" ").split("|");
+  const cmd = await context.commands.get(cmdName);
+  if (cmd) {
+    if (cmd.handler === "text") {
+      if (args.length > 0) {
+        cmd.args = args.join(" ").split("|");
         context.commands.set(cmdName, cmd);
       }
-      else if (cmd.handler === "counter") {
-        if (args.length === 0) {
-          cmd.args[1] += 1;
-          context.commands.set(cmdName, cmd);
-        }
-        else if (args.length === 1) {
-          const num = parseInt(args[0]);
-          if (!Number.isNaN(num)) {
-            cmd.args[1] = num;
-          }
+    }
+    else if (cmd.handler === "counter") {
+      if (args.length === 0) {
+        cmd.args[1] += 1;
+        context.commands.set(cmdName, cmd);
+      }
+      else if (args.length === 1) {
+        const num = parseInt(args[0]);
+        if (!Number.isNaN(num)) {
+          cmd.args[1] = num;
         }
       }
     }
