b2acbb569aac726bb24fd06e483509ea3ecf1399
Vadim Alimguzhin <vadim.alimguzhin@gmail.com>
2022-08-27 19:00:48 +0200

    Adjust counter output.

--- counter.js
+++ counter.js
@@ -1,6 +1,6 @@
 const pluralize = require("pluralize");
 
 module.exports = async (context, text, num) => {
-  const numStr = pluralize("time", num);
+  const numStr = pluralize("time", num, true);
   await context.chat.say(context.channel.displayName, `${text} ${numStr}.`);
 };
