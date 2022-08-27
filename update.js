30e684ce218c758024f3a4af9914b7bb041fe896
Vadim Alimguzhin <vadim.alimguzhin@gmail.com>
2022-08-27 18:42:04 +0200

    Enable update command to work with counters.

--- update.js
+++ update.js
@@ -1,9 +1,23 @@
-module.exports = async (context, cmdName, ...text) => {
+module.exports = async (context, cmdName, ...args) => {
   if (text.length > 0) {
     const cmd = await context.commands.get(cmdName);
-    if (cmd && cmd.handler === "text") {
-      cmd.args = text.join(" ").split("|");
-      context.commands.set(cmdName, cmd);
+    if (cmd) {
+      if (cmd.handler === "text") {
+        cmd.args = text.join(" ").split("|");
+        context.commands.set(cmdName, cmd);
+      }
+      else if (cmd.handler === "counter") {
+        if (args.length === 0) {
+          cmd.args[1] += 1;
+          context.commands.set(cmdName, cmd);
+        }
+        else if (args.length === 1) {
+          const num = parseInt(args[0]);
+          if (!Number.isNaN(num)) {
+            cmd.args[1] = num;
+          }
+        }
+      }
     }
   }
 };
