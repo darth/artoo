20cfa69c1a2c6ea6e8dd0ed7defd88e860f54cd1
Vadim Alimguzhin <vadim.alimguzhin@gmail.com>
2023-05-13 17:08:09 +0200

    Fix pd2 armory handler.

--- pd2_armory.js
+++ pd2_armory.js
@@ -1,6 +1,3 @@
 module.exports = async (context, ...args) => {
   await context.chat.say(context.channel.displayName, `https://projectdiablo2.com/character/${args[0]}`);
-  for (const arg of args) {
-    await context.chat.say(context.channel.displayName, arg);
-  }
 };
